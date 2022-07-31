f370c30fabac5aa2a2c2cbc0a1229dcc377dfd3e
“rqchta” <rob.kuchta@gmail.com>
2022-07-31 16:56:37 +0200

    After cloning

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,45 +1,46 @@
-import React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
-import axios from 'axios';
+import React from "react"
+import StripeCheckout from "react-stripe-checkout"
+import axios from "axios"
 
 const StripeCheckoutButton = ({ price }) => {
-  const priceForStripe = price * 100;
-  const publishableKey = 'pk_test_b7a3hFL5nC3qlBCZ6bQACpez00gyMMP52H';
+  const priceForStripe = price * 100
+  const publishableKey =
+    "pk_test_51LMvlWCPJtlZhVJmToTkOMgDFmWnb1gk8lFYvpIghjrRPMLOu4Yx8mJWU1e3ICQeEGVRO4VQfWNUnMcaapMIY6sV00BIKr1J4S"
 
   const onToken = token => {
     axios({
-      url: 'payment',
-      method: 'post',
+      url: "payment",
+      method: "post",
       data: {
         amount: priceForStripe,
         token: token
       }
     })
       .then(response => {
-        alert('succesful payment');
+        alert("succesful payment")
       })
       .catch(error => {
-        console.log('Payment Error: ', error);
+        console.log("Payment Error: ", error)
         alert(
-          'There was an issue with your payment! Please make sure you use the provided credit card.'
-        );
-      });
-  };
+          "There was an issue with your payment! Please make sure you use the provided credit card."
+        )
+      })
+  }
 
   return (
     <StripeCheckout
-      label='Pay Now'
-      name='CRWN Clothing Ltd.'
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
       billingAddress
       shippingAddress
-      image='https://svgshare.com/i/CUz.svg'
+      image="https://svgshare.com/i/CUz.svg"
       description={`Your total is $${price}`}
       amount={priceForStripe}
-      panelLabel='Pay Now'
+      panelLabel="Pay Now"
       token={onToken}
       stripeKey={publishableKey}
     />
-  );
-};
+  )
+}
 
-export default StripeCheckoutButton;
+export default StripeCheckoutButton
--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -1,91 +1,87 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-import 'firebase/auth';
+import firebase from "firebase/app"
+import "firebase/firestore"
+import "firebase/auth"
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
-};
+  apiKey: "AIzaSyCSGpFfOU7bs65ImgdmoWxGRF8jAEz93YM",
+  authDomain: "tfu-clothing-db.firebaseapp.com",
+  projectId: "tfu-clothing-db",
+  storageBucket: "tfu-clothing-db.appspot.com",
+  messagingSenderId: "1050410086806",
+  appId: "1:1050410086806:web:903e90858481770fc8adb7"
+}
 
-firebase.initializeApp(config);
+firebase.initializeApp(config)
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-  if (!userAuth) return;
+  if (!userAuth) return
 
-  const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
 
-  const snapShot = await userRef.get();
+  const snapShot = await userRef.get()
 
   if (!snapShot.exists) {
-    const { displayName, email } = userAuth;
-    const createdAt = new Date();
+    const { displayName, email } = userAuth
+    const createdAt = new Date()
     try {
       await userRef.set({
         displayName,
         email,
         createdAt,
         ...additionalData
-      });
+      })
     } catch (error) {
-      console.log('error creating user', error.message);
+      console.log("error creating user", error.message)
     }
   }
 
-  return userRef;
-};
+  return userRef
+}
 
-export const addCollectionAndDocuments = async (
-  collectionKey,
-  objectsToAdd
-) => {
-  const collectionRef = firestore.collection(collectionKey);
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey)
 
-  const batch = firestore.batch();
+  const batch = firestore.batch()
   objectsToAdd.forEach(obj => {
-    const newDocRef = collectionRef.doc();
-    batch.set(newDocRef, obj);
-  });
+    const newDocRef = collectionRef.doc()
+    batch.set(newDocRef, obj)
+  })
 
-  return await batch.commit();
-};
+  return await batch.commit()
+}
 
 export const convertCollectionsSnapshotToMap = collections => {
   const transformedCollection = collections.docs.map(doc => {
-    const { title, items } = doc.data();
+    const { title, items } = doc.data()
 
     return {
       routeName: encodeURI(title.toLowerCase()),
       id: doc.id,
       title,
       items
-    };
-  });
+    }
+  })
 
   return transformedCollection.reduce((accumulator, collection) => {
-    accumulator[collection.title.toLowerCase()] = collection;
-    return accumulator;
-  }, {});
-};
+    accumulator[collection.title.toLowerCase()] = collection
+    return accumulator
+  }, {})
+}
 
 export const getCurrentUser = () => {
   return new Promise((resolve, reject) => {
     const unsubscribe = auth.onAuthStateChanged(userAuth => {
-      unsubscribe();
-      resolve(userAuth);
-    }, reject);
-  });
-};
+      unsubscribe()
+      resolve(userAuth)
+    }, reject)
+  })
+}
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+export const auth = firebase.auth()
+export const firestore = firebase.firestore()
 
-export const googleProvider = new firebase.auth.GoogleAuthProvider();
-googleProvider.setCustomParameters({ prompt: 'select_account' });
-export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
+export const googleProvider = new firebase.auth.GoogleAuthProvider()
+googleProvider.setCustomParameters({ prompt: "select_account" })
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider)
 
-export default firebase;
+export default firebase
